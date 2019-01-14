aa536b5e734d78baf3a9d4e9110d14d004216ace
yajash <yajash989@gmail.com>
2019-01-14 17:09:20 +0530

    Updates

---
+++ precache-manifest.f226f760235959cf7b04ccc3daf6b737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFreinds/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "61b449c2d2b6213b68a2",
+    "url": "/RoboFreinds/static/js/runtime~main.61b449c2.js"
+  },
+  {
+    "revision": "96519da096dfe27a67bc",
+    "url": "/RoboFreinds/static/js/main.96519da0.chunk.js"
+  },
+  {
+    "revision": "89c8111383c1c0c556d7",
+    "url": "/RoboFreinds/static/js/1.89c81113.chunk.js"
+  },
+  {
+    "revision": "96519da096dfe27a67bc",
+    "url": "/RoboFreinds/static/css/main.cdb1bbf3.chunk.css"
+  },
+  {
+    "revision": "89c8111383c1c0c556d7",
+    "url": "/RoboFreinds/static/css/1.5b1afbd3.chunk.css"
+  },
+  {
+    "revision": "46bee1f67cfdbe294954790b8ddd2c0a",
+    "url": "/RoboFreinds/index.html"
+  }
+];
