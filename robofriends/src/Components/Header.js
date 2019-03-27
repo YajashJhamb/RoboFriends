6cc4e4ffc1379d254665c39a3fbfebcea2fced4a
yajash <yajash989@gmail.com>
2019-03-27 13:24:42 +0530

    made changes

--- robofriends/src/Components/Header.js
+++ robofriends/src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className="f1">RoboFriends</h1>
-      <Link className="btn-primary" to="/add-robot">
+      <Link className="btn-primary p-3 m3" to="/add-robot">
         {" "}
         ADD ROBOT
       </Link>
